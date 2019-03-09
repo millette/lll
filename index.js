62bb8b45da52bfea7d985a1c4f1db038b544a9b9
Robin Millette <robin@millette.info>
2019-03-09 14:13:42 -0500

    Create dir as required.

--- index.js
+++ index.js
@@ -12,11 +12,13 @@ const LevelErrors = require("level-errors")
 const through = require("through2")
 const Ajv = require("ajv")
 const localize = require("ajv-i18n/localize/fr")
+const mkdir = require("make-dir")
 
 const leveldownDestroy = promisify(leveldown.destroy)
 const itKeys = ["gt", "gte", "lt", "lte", "start", "end"]
 const POST_END = "\ufff0"
 const defaultAjv = { allErrors: true, verbose: true }
+const schemaSchema = {}
 
 const getDb = (loc, options = {}) => {
   assert.equal(loc && typeof loc, "string", "loc argument must be a string.")
@@ -120,6 +122,8 @@ const getDb = (loc, options = {}) => {
       db.off("error", reject)
       this.db = db
       this.ajv = ajv
+
+      this.schemas = new Table(this, "_table", schemaSchema)
     }
 
     getTable(name) {
@@ -150,30 +154,33 @@ const getDb = (loc, options = {}) => {
     }
   }
 
-  return new Promise((resolve, reject) => {
-    const db = leveldown(loc)
-    let levelOptions
-    const { level, ajv, ...rest } = options
-    if (!level && !ajv) levelOptions = rest
-    if (level) levelOptions = level
-    const ajvOptions = {
-      ...defaultAjv,
-      ...(ajv || {}),
-    }
+  const gogo = () =>
+    new Promise((resolve, reject) => {
+      const db = leveldown(loc)
+      let levelOptions
+      const { level, ajv, ...rest } = options
+      if (!level && !ajv) levelOptions = rest
+      if (level) levelOptions = level
+      const ajvOptions = {
+        ...defaultAjv,
+        ...(ajv || {}),
+      }
 
-    db.open(levelOptions, (e) => {
-      if (e) return reject(e)
-      db.close(() => {
-        const db2 = levelup(encode(db, { valueEncoding: "json" }))
-        const ok = () => resolve(new Tada(db2, reject, new Ajv(ajvOptions)))
-        db2.once("ready", ok)
-        db2.once("error", (err) => {
-          db2.off("ready", ok)
-          reject(err)
+      db.open(levelOptions, (e) => {
+        if (e) return reject(e)
+        db.close(() => {
+          const db2 = levelup(encode(db, { valueEncoding: "json" }))
+          const ok = () => resolve(new Tada(db2, reject, new Ajv(ajvOptions)))
+          db2.once("ready", ok)
+          db2.once("error", (err) => {
+            db2.off("ready", ok)
+            reject(err)
+          })
         })
       })
     })
-  })
+
+  return mkdir(loc).then(gogo)
 }
 
 module.exports = getDb
