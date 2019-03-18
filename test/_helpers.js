8173aa3734a7be99ed8c0f992780fdd8f1d8308f
Robin Millette <robin@millette.info>
2019-03-18 16:34:40 -0400

    Convert helpers from commonjs to es module.

--- test/_helpers.js
+++ test/_helpers.js
@@ -8,40 +8,15 @@ import { mkdtemp } from "fs"
 // npm
 import del from "del"
 
-export default {
-  beforeEach: (t) =>
-    new Promise((resolve, reject) => {
-      mkdtemp(`${tmpdir}${sep}`, (err, folder) => {
-        if (err) return reject(err)
-        t.context.loc = folder
-        resolve()
-      })
-    }),
-  afterEach: (t) => {
-    if (t.context.loc) return del(`${t.context.loc}${sep}**`, { force: true })
-  },
+export const beforeEach = (t) =>
+  new Promise((resolve, reject) => {
+    mkdtemp(`${tmpdir}${sep}`, (err, folder) => {
+      if (err) return reject(err)
+      t.context.loc = folder
+      resolve()
+    })
+  })
+
+export const afterEach = (t) => {
+  if (t.context.loc) return del(`${t.context.loc}${sep}**`, { force: true })
 }
-
-/*
-// core
-const { sep } = require("path")
-const { tmpdir } = require("os")
-const { mkdtemp } = require("fs")
-
-// npm
-const del = require("del")
-
-module.exports = {
-  beforeEach: (t) =>
-    new Promise((resolve, reject) => {
-      mkdtemp(`${tmpdir}${sep}`, (err, folder) => {
-        if (err) return reject(err)
-        t.context.loc = folder
-        resolve()
-      })
-    }),
-  afterEach: (t) => {
-    if (t.context.loc) return del(`${t.context.loc}${sep}**`, { force: true })
-  },
-}
-*/
