5b2d884c531f5b3221a6d1e989b0314de3dde13d
Robin Millette <robin@millette.info>
2019-03-21 09:20:34 -0400

    Basic access tests.

--- test/test-acl.js
+++ test/test-acl.js
@@ -10,20 +10,25 @@ import { beforeEach, afterEach } from "./_helpers"
 test.beforeEach(beforeEach)
 test.afterEach.always(afterEach)
 
-test("acl", async (t) => {
+test("acl get, user required", async (t) => {
   const db = await getDb(t.context.loc, { errorIfExists: true })
-
-  const access = {
-    put: false,
-  }
-
+  const access = { get: Boolean }
   const t1 = await db.createTable("bobo", { access })
-
   await t1.put("joe", "blow")
+  t.throwsAsync(t1.get("joe"), { message: "Cannot get." })
+  const v = await t1.get("joe", "ron")
+  t.is(v, "blow")
+  await db.destroy()
+  t.pass()
+})
 
-  const i1 = await t1.get("joe")
-  console.log("i1:", i1)
+test("acl put, user required", async (t) => {
+  const db = await getDb(t.context.loc, { errorIfExists: true })
+  const access = { put: Boolean }
+  const t1 = await db.createTable("bobo", { access })
+  t.throwsAsync(t1.put("joe", "blow"), { message: "Cannot put." })
 
+  await t1.put("joe", "blow", "ron")
   await db.destroy()
   t.pass()
 })
