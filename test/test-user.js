a7f1f8da4ffe900554c8ad8b44fce966e6777645
Robin Millette <robin@millette.info>
2019-03-26 15:53:30 -0400

    More user unit tests.

--- test/test-user.js
+++ test/test-user.js
@@ -4,7 +4,7 @@
 import test from "ava"
 
 // self
-import getDb from ".."
+import getDb, { LevelErrors } from ".."
 import { beforeEach, afterEach } from "./_helpers"
 
 test.beforeEach(beforeEach)
@@ -13,19 +13,33 @@ test.afterEach.always(afterEach)
 test("create user and destroy db", async (t) => {
   const password = "elPassword"
   const badPassword = "elPassword666"
+  const _id = "b-ob"
+  const badId = "ji-m"
 
   const db = await getDb(t.context.loc, { errorIfExists: true })
   const users = db.getUsers()
-  await users.register({ _id: "b-ob", password })
-  await users.get("b-ob")
+  await users.register({ _id, password })
 
-  await users.login({ _id: "b-ob", password })
+  await users.login({ _id, password })
 
-  t.throwsAsync(() => users.register({ _id: "b-ob", password }), {
+  t.throwsAsync(() => users.register({ _id, password }), {
     message: "User already exists.",
   })
-  t.throwsAsync(() => users.login({ _id: "b-ob", password: badPassword }), {
+
+  t.throwsAsync(() => users.get(_id), {
+    message: "UserTable.get() is not implemented.",
+  })
+
+  t.throwsAsync(() => users.put({ _id, password }), {
+    message: "UserTable.put() is not implemented.",
+  })
+
+  t.throwsAsync(() => users.login({ _id, password: badPassword }), {
     message: "Password does not match.",
+  })
+
+  t.throwsAsync(() => users.login({ _id: badId, password }), {
+    instanceOf: LevelErrors.NotFoundError,
   })
 
   await db.destroy()
